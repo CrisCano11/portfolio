dec6814145902f1ca58820ad3140cce0e095487d
Cristhian Cano <criscano_1994@hotmail.com>
2023-11-20 23:17:03 -0500

    project data

--- src/context/ProjectsContext.jsx
+++ src/context/ProjectsContext.jsx
@@ -1,44 +1,48 @@
-import { useState, createContext } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import { projectsData } from '../data/projects';
+import useDetectLanguage from '../hooks/useDetectLanguage';
 
 export const ProjectsContext = createContext();
 
 export const ProjectsProvider = (props) => {
-	const [projects, setProjects] = useState(projectsData);
-	const [searchProject, setSearchProject] = useState('');
-	const [selectProject, setSelectProject] = useState('');
+    const [projects, setProjects] = useState(projectsData.english);
+    const [searchProject, setSearchProject] = useState('');
+    const [selectProject, setSelectProject] = useState('');
+    const isSpanish = useDetectLanguage();
 
-	const searchProjectsByTitle = projects.filter((item) => {
-		const result = item.title
-			.toLowerCase()
-			.includes(searchProject.toLowerCase())
-			? item
-			: searchProject === ''
-			? item
-			: '';
-		return result;
-	});
+   
+    useEffect(() => {
+        const data = isSpanish ? projectsData.spanish : projectsData.english;
+        setProjects(data);
+    }, [isSpanish]);
 
-	const selectProjectsByCategory = projects.filter((item) => {
-		let category =
-			item.category.charAt(0).toUpperCase() + item.category.slice(1);
-		return category.includes(selectProject);
-	});
+    const searchProjectsByTitle = projects.filter((item) => {
+        const result =
+            item.title.toLowerCase().includes(searchProject.toLowerCase()) ||
+            searchProject === '';
+        return result;
+    });
 
-	return (
-		<ProjectsContext.Provider
-			value={{
-				projects,
-				setProjects,
-				searchProject,
-				setSearchProject,
-				searchProjectsByTitle,
-				selectProject,
-				setSelectProject,
-				selectProjectsByCategory,
-			}}
-		>
-			{props.children}
-		</ProjectsContext.Provider>
-	);
+    const selectProjectsByCategory = projects.filter((item) => {
+        let category =
+            item.category.charAt(0).toUpperCase() + item.category.slice(1);
+        return category.includes(selectProject);
+    });
+
+    return (
+        <ProjectsContext.Provider
+            value={{
+                projects,
+                setProjects,
+                searchProject,
+                setSearchProject,
+                searchProjectsByTitle,
+                selectProject,
+                setSelectProject,
+                selectProjectsByCategory,
+            }}
+        >
+            {props.children}
+        </ProjectsContext.Provider>
+    );
 };
--- src/data/projects.js
+++ src/data/projects.js
@@ -4,44 +4,89 @@ import Cerdos from '../images/cerdos_3.webp';
 import Bosque from '../images/bosque_1.webp';
 import Pwa from '../images/pwa_1.webp';
 
-export const projectsData = [
-	{
-		id: 1,
-		title: 'Field Visit and Data Management System',
-		category: 'Web Application',
-		img: IDM,
-	},
-	{
-		id: 2,
-		title: 'POS System',
-		category: 'Web Application',
-		img: Cerdos,
-        ProjectHeader: {
-			title: 'Project Management UI - From Context',
-			publishDate: 'Apr 20, 2023',
-			tags: 'Administration / Backend',
-		},
-	},
-	{
-		id: 3,
-		title: 'POS System for Restaurant',
-		category: 'Web Application',
-		img: Bosque,
-        ProjectHeader: {
-			title: 'Project Management UI - From Context',
-			publishDate: 'Jul 2, 2022',
-			tags: 'Administration / Backend',
-		},
-	},
-	{
-		id: 4,
-		title: 'Login for Platform',
-		category: 'Progressive Web Application (PWA)',
-		img: Pwa,
-        ProjectHeader: {
-			title: 'Project Management UI - From Context',
-			publishDate: 'May 3, 2023',
-			tags: 'UI / Frontend',
-		},
-	}
-];
+export const projectsData =
+{
+    english: [
+        {
+            id: 1,
+            title: 'Field Visit and Data Management System',
+            category: 'Web Application',
+            img: IDM,
+        },
+        {
+            id: 2,
+            title: 'POS System',
+            category: 'Web Application',
+            img: Cerdos,
+            ProjectHeader: {
+                title: 'Project Management UI - From Context',
+                publishDate: 'Apr 20, 2023',
+                tags: 'Administration / Backend',
+            },
+        },
+        {
+            id: 3,
+            title: 'POS System for Restaurant',
+            category: 'Web Application',
+            img: Bosque,
+            ProjectHeader: {
+                title: 'Project Management UI - From Context',
+                publishDate: 'Jul 2, 2022',
+                tags: 'Administration / Backend',
+            },
+        },
+        {
+            id: 4,
+            title: 'Login for Platform',
+            category: 'Progressive Web Application (PWA)',
+            img: Pwa,
+            ProjectHeader: {
+                title: 'Project Management UI - From Context',
+                publishDate: 'May 3, 2023',
+                tags: 'UI / Frontend',
+            },
+        }
+    ],
+    spanish: [
+        {
+            id: 1,
+            title: 'Sistema de gestión de datos y visita de campo',
+            category: 'Aplicación Web/Proyecto de Grado',
+            img: IDM,
+        },
+        {
+            id: 2,
+            title: 'Sistema POS',
+            category: 'Aplicación Web',
+            img: Cerdos,
+            ProjectHeader: {
+                title: 'UI de gestión de proyectos - Desde el contexto',
+                publishDate: 'Abr 20, 2023',
+                tags: 'Administration / Backend',
+            },
+        },
+        {
+            id: 3,
+            title: 'Sistema POS para Restaurante',
+            category: 'Aplicación Web',
+            img: Bosque,
+            ProjectHeader: {
+                title: 'UI de gestión de proyectos - Desde el contexto',
+                publishDate: 'Jul 2, 2022',
+                tags: 'Administration / Backend',
+            },
+        },
+        {
+            id: 4,
+            title: 'Inicio  de sesión para plataforma',
+            category: 'Aplicación Web Progresiva (PWA)',
+            img: Pwa,
+            ProjectHeader: {
+                title: 'UI de gestión de proyectos - Desde el contexto',
+                publishDate: 'May 3, 2023',
+                tags: 'UI / Frontend',
+            },
+        }
+    ]
+};
+
