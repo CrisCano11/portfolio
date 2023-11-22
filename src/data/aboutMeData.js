a5e0855cc7ef83b97ca793c10e72560a1ae4c220
Cristhian Cano <criscano_1994@hotmail.com>
2023-11-22 17:16:58 -0500

    data for about me and clients

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,10 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: 'Ingeniero de sistemas con experiencia en el desarrollo de software realizando el levantamiento de requerimientos, diseño, instalación, pruebas y mantenimiento. Dominio de varios lenguajes y sistemas. Capaz de autogestión y eficaz en proyectos independientes. Buenas habilidades de colaboración dentro de un equipo productivo.',
+	},
+	{
+		id: 2,
+		bio: 'Como Desarrollador Web Full Stack, estoy encargado de crear y mantener aplicaciones web de alto rendimiento y experiencias de usuario excepcionales. responsable de la implementación de diseños y funcionalidades que mejoren la interacción de los usuarios con nuestras plataformas en línea. realizando recopilación de requisitos, diseño, instalación, pruebas y mantenimiento',
+	},
+];
---
+++ src/data/clientsData.jsx
@@ -0,0 +1,34 @@
+ import ChikRosa from '../images/brands/chik_rosa.webp'
+ import Cerdos from '../images/brands/cerdos.webp'
+ import Bosque from '../images/brands/el_bosque.webp'
+ import Saberes from '../images/brands/sistema_saberes.webp'
+ import Cerok from '../images/brands/cero_k.webp'
+
+
+export const clientsData = [
+	{
+		id: 1,
+		title: 'Cerdos del Eje',
+		img: Cerdos,
+	},
+	{
+		id: 2,
+		title: 'Pesca Deportiva y Balneario el Bosque',
+		img: Bosque,
+	},
+	{
+		id: 3,
+		title: 'Chik Rosa Boutique',
+		img: ChikRosa
+	},
+	{
+		id: 4,
+		title: 'Grupo Saberes S.A.S',
+		img: Saberes,
+	},
+	{
+		id: 5,
+		title: 'Cero k S.A.S',
+		img: Cerok,
+	}
+];
