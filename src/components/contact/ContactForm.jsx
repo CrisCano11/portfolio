c8cad0eacdf188b55dee36c466f4ab7888e21b9a
Cristhian Cano <criscano_1994@hotmail.com>
2024-01-04 16:07:20 -0500

    fix problem with show errors diferent from form

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -134,7 +134,7 @@ const ContactForm = () => {
 									aria-label="Message"
 								></textarea>
 							</div>
-							{errors["-"] && (
+							{errors["other"] && (
 								<div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
 									<strong className="font-bold">Error! </strong>
 									<span className="block sm:inline">{errors["other"]}</span>
